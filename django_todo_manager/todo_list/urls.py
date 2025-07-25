4ee4ba10a3e0b393c0f95dd411b01a0dfeb613e9
Евгений Бусыгин (ПК) <abusyginza@gmail.com>
2025-07-25 08:10:43 +0700

    views

--- django_todo_manager/todo_list/urls.py
+++ django_todo_manager/todo_list/urls.py
@@ -2,6 +2,11 @@ from django.contrib import admin
 from django.urls import path, include
 from django.views.generic import TemplateView
 
+from .views import index_view
+
+app_name = "todo_list"
+
 urlpatterns = [
-    path("", TemplateView.as_view(template_name="todo_list/index.html"), name='index'), 
+    path("", index_view, name='index'), 
 ]
+ 
--- django_todo_manager/todo_list/views.py
+++ django_todo_manager/todo_list/views.py
@@ -1,3 +1,16 @@
+import re
 from django.shortcuts import render
+from django.http import HttpRequest
 
-# Create your views here.
+
+
+def index_view(request: HttpRequest):
+    todo_items = [
+        "Item 1",
+        "Item 2",
+    ]
+    return render(request=request,
+                  template_name="todo_list/index.html",
+                  context={"todo_items": todo_items},
+                  )
+
